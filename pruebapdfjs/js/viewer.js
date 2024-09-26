5ceb3d2b9c5ed20dd8cd6b7489389ea651fe6f3d
Juanes <juanastaizafuenmayor@gmail.com>
2024-09-26 13:27:55 -0500

    first commit

---
+++ pruebapdfjs/js/viewer.js
@@ -0,0 +1,26 @@
+const url = 'https://drive.google.com/uc?export=download&id=1Ibi_bI1xtk70KCX2yzrhYGfa5Psr5bhu';
+
+const pdfjsLib = window['pdfjs-dist/build/pdf'];
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.worker.min.js';
+
+const canvas = document.getElementById('pdf-render');
+const ctx = canvas.getContext('2d');
+
+pdfjsLib.getDocument(url).promise.then(pdfDoc => {
+    pdfDoc.getPage(1).then(page => {
+        const viewport = page.getViewport({ scale: 1.5 });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        const renderContext = {
+            canvasContext: ctx,
+            viewport: viewport
+        };
+
+        page.render(renderContext);
+    }).catch(error => {
+        console.error('Error rendering page:', error);
+    });
+}).catch(error => {
+    console.error('Error loading PDF:', error);
+});
